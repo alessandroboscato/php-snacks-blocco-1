da6375cea8416cf2c7ca850b29be01703dec025b
Alessandro <alessandro.boscato@gmail.com>
2020-10-05 14:48:55 +0200

    Inizip progetto

---
+++ index.php
@@ -0,0 +1,10 @@
+<!-- PHP Snack 1:
+Creiamo un array 'matches' contenente altri array i quali rappresentano delle partite di basket di un’ipotetica tappa del calendario. Ogni array della partita avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite.
+Stampiamo a schermo tutte le partite con questo schema:
+Olimpia Milano - Cantù | 55-60
+PHP Snack 2:
+Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che:
+1. name sia più lungo di 3 caratteri,
+2. che mail contenga un punto e una chiocciola
+3. e che age sia un numero.
+Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
