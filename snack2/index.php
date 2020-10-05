8f98ad998492c9ce97f90a0bb147ef6cb4795c01
Alessandro <alessandro.boscato@gmail.com>
2020-10-05 18:02:08 +0200

    Fine snack 1 e 2

---
+++ snack2/index.php
@@ -0,0 +1,36 @@
+<!-- PHP Snack 2:
+Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che:
+1. name sia più lungo di 3 caratteri,
+2. che mail contenga un punto e una chiocciola
+3. e che age sia un numero.
+Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
+
+<?php
+//snack 2
+$name = $_GET["name"];
+// controlla che name sia più lungo di 3 caratteri,
+$mail = $_GET["mail"];
+// controlla che mail contenga un punto e una chiocciola
+$age = $_GET["age"];
+//controlla che age sia un numero
+$user_data = "";
+
+if (strlen($name) >= 3 && !is_nan($name)) {
+ echo($name);
+ $user_data .= $name. " ";
+
+}
+
+if (strpos($mail, ".") !== false && strpos($mail, "@") !== false) {
+ echo($mail);
+ $user_data .= $mail. " ";
+}
+
+if (is_numeric($age) && $age > 0) {
+ echo($age);
+ $user_data .= $age. " ";
+}
+
+?>
+
+<h1>Dati inseriti dall'utente: <?php echo $user_data?></h1>
